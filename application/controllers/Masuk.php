8dae4bd6cb21ee14ac38b2608732bf8eb55cc2d7
afifhusain <afifhusain2014@gmail.com>
2020-02-21 17:54:18 +0700

    perubahan

--- application/controllers/Masuk.php
+++ application/controllers/Masuk.php
@@ -3,25 +3,29 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Masuk extends CI_Controller {
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->library('form_validation');
+	}
+
 	public function index()
 	{
 		$this->load->view('desain/header');
 		$this->load->view('masuk/index');
 		$this->load->view('desain/footer');
 	}
+
+	public function register()
+	{
+		$this->form_validation->set_rules('username', 'username', 'required|trim');
+		$this->form_validation->set_rules('email', 'email', 'required|trim|valid_email|unik [user.email]', [
+			'unik' => 'email telah terdaftar'
+		]);
+
+		$this=>
+	}
+
+
+
 }
--- application/views/masuk/index.php
+++ application/views/masuk/index.php
@@ -1,7 +1,10 @@
-<div class="container">
+
+<div class="main">
+
+<div class="container center">
 
 
-<div class="col s12 m4">
+<div class="col s12">
           <div class="icon-block">
             <h2 class="center light-blue-text"><i class="material-icons">group</i></h2>
             <h5 class="center">Halaman Register Akun</h5>
@@ -9,35 +12,45 @@
             <p class="light"></p>
           </div>
         </div>
+ 
 
 
 
 
-  <div class="row mt-9">
-    <form>
-      <div class="row">
-        <div class="input-field col s6">
+    <form method="post" action="<?= base_url();?>/masuk/registrasi">
+   
+        <div class="input-field">
           <i class="material-icons prefix">account_circle</i>
           <input id="username" type="text" class="validate" name="username">
           <label for="icon_prefix">Username</label>
         </div>
 
 
-        <div class="input-field col s9">
+        <div class="input-field">
           <i class="material-icons prefix">email</i>
           <input id="email" type="email" class="validate" name="email">
           <label for="icon_telephone">Email</label>
         </div>
 
-         <div class="input-field col s9">
+         <div class="input-field">
           <i class="material-icons prefix">vpn_key</i>
           <input id="password" name="password" type="password" class="validate">
           <label for="icon_telephone">Password</label>
         </div>    
 
-      </div>
-      <a class="waves-effect waves-light btn-small"><i class="material-icons left">cloud</i>Register Akun</a>
+        <div class="input-field">
+          <i class="material-icons prefix">vpn_key</i>
+          <input id="password2" name="password2" type="password" class="validate">
+          <label for="icon_telephone">Confirm Password</label>
+        </div>    
+
+
+
+
+     
+      <button class="waves-effect waves-light btn-small" type="submit"><i class="material-icons left">cloud</i>Register Akun</button>
 
     </form>
-  </div>
+  
+   </div>
    </div>
